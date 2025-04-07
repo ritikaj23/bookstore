8412bcac29605dd978e727940c9452e8e985a191
ritikaj23 <ritikaj@skillup.online>
2025-04-07 06:11:24 -0400

    modified requirements.txt

---
+++ code/src/main/java/com/project/code/Book.java
@@ -0,0 +1,65 @@
+package com.project.code;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+    private String genre;
+
+    // Getter for id
+    public Long getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter for title
+    public String getTitle() {
+        return title;
+    }
+
+    // Setter for title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter for author
+    public String getAuthor() {
+        return author;
+    }
+
+    // Setter for author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Getter for price
+    public double getPrice() {
+        return price;
+    }
+
+    // Setter for price
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter for genre
+    public String getGenre() {
+        return genre;
+    }
+
+    // Setter for genre
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
